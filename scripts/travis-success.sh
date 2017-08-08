e7ee82be046a4768530258c6af09bde465b38b6f
Gietson <gietson1@gmail.com>
2017-08-08 12:47:40 +0200

    configure travis

--- scripts/travis-success.sh
+++ scripts/travis-success.sh
@@ -1,2 +1,2 @@
-dotnet publish ./TestTravisCI -c Release -o './bin/Release/netcoreapp1.1'
-curl -T './bin/Release/netcoreapp1.1' -u $FTP_USER:$FTP_PASSWORD ftp://213.218.125.222
+dotnet publish ./TestTravisCI -c Release -o ./bin/Release/netcoreapp1.1
+curl -T ./bin/Release/netcoreapp1.1 -u $FTP_USER:$FTP_PASSWORD ftp://213.218.125.222
